227a7265cdb54ff0ad54f7963dc416ae4b0f30e4
NasserObm <nassermohamedobame@gmail.com>
2024-11-27 04:59:37 +0100

    Page2

--- lib/Page2.dart
+++ lib/Page2.dart
@@ -1,6 +1,9 @@
 // ignore_for_file: file_names
 
+import 'package:fl_chart/fl_chart.dart';
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+import 'package:stylish_bottom_bar/stylish_bottom_bar.dart';
 
 class Page2 extends StatefulWidget {
   const Page2({super.key});
@@ -9,12 +12,466 @@ class Page2 extends StatefulWidget {
   State<Page2> createState() => _Page2State();
 }
 
+Widget createCustomCard({
+  required String avatarImage,
+  required String name,
+  required String email,
+  required String amount,
+  required String transactionId,
+  required String date,
+}) {
+  return Padding(
+    padding: const EdgeInsets.all(8.0),
+    child: Row(
+      children: [
+        Container(
+          width: 350,
+          height: 200,
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(10),
+            color: Colors.white,
+          ),
+          child: Column(
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Row(
+                    children: [
+                      CircleAvatar(
+                        backgroundImage: AssetImage(avatarImage),
+                      ),
+                      Column(
+                        children: [
+                          Text(
+                            name,
+                            style: const TextStyle(
+                              fontSize: 18,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                          Text(
+                            email,
+                            style: const TextStyle(
+                              fontSize: 11,
+                              fontWeight: FontWeight.normal,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                  const Icon(
+                    Icons.arrow_forward_ios_sharp,
+                    color: Colors.black,
+                  ),
+                ],
+              ),
+              Container(
+                width: 290,
+                height: 70,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(10),
+                  color: const Color(0xffF5F9F9),
+                ),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          amount,
+                          style: const TextStyle(
+                            fontSize: 24,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                        const Text(
+                          "Amount",
+                          style: TextStyle(
+                            fontSize: 12,
+                            fontWeight: FontWeight.normal,
+                          ),
+                        ),
+                      ],
+                    ),
+                    ElevatedButton(
+                      onPressed: () {},
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: const Color(0xffCAECCD),
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(10),
+                        ),
+                      ),
+                      child: const Text(
+                        "Paid",
+                        style: TextStyle(
+                          color: Color(0xff73D173),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              Row(
+                children: [
+                  Text('#$transactionId '),
+                  const Icon(
+                    Icons.circle,
+                    color: Color(0xff878787),
+                    size: 7,
+                  ),
+                  Text(date),
+                ],
+              ),
+            ],
+          ),
+        ),
+      ],
+    ),
+  );
+}
+
 class _Page2State extends State<Page2> {
+  int _selectedIndex = 0;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: const Color(0xffECF0F1),
-      body: ListView(),
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 20),
+        child: ListView(
+          children: [
+            const Row(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                Image(image: AssetImage("assets/trads-2.jpg")),
+                SizedBox(
+                  width: 45,
+                ),
+                Icon(
+                  Icons.notification_add_outlined,
+                  color: Color(0xff2CD196),
+                ),
+              ],
+            ),
+            const Row(
+              children: [
+                Text(
+                  'Overview',
+                  style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+                ),
+                Spacer(),
+                Icon(Icons.menu),
+              ],
+            ),
+            const SizedBox(height: 20),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                // Graphique
+                Container(
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                  child: Center(
+                    child: Container(
+                      width: 250,
+                      height: 250,
+                      padding: const EdgeInsets.all(20),
+                      decoration: BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: BorderRadius.circular(20),
+                      ),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          const Text(
+                            '€53,200',
+                            style: TextStyle(
+                                fontSize: 25, fontWeight: FontWeight.bold),
+                          ),
+                          const Text(
+                            'Received',
+                            style: TextStyle(fontSize: 16, color: Colors.grey),
+                          ),
+                          const SizedBox(height: 10),
+                          Expanded(
+                              child: LineChart(
+                            LineChartData(
+                              gridData: const FlGridData(show: false),
+                              titlesData: FlTitlesData(
+                                leftTitles: const AxisTitles(
+                                  sideTitles: SideTitles(showTitles: false),
+                                ),
+                                rightTitles: const AxisTitles(
+                                  sideTitles: SideTitles(showTitles: false),
+                                ),
+                                topTitles: const AxisTitles(
+                                  sideTitles: SideTitles(showTitles: false),
+                                ),
+                                bottomTitles: AxisTitles(
+                                  sideTitles: SideTitles(
+                                    showTitles: false,
+                                    getTitlesWidget: (value, meta) {
+                                      final months = [
+                                        'Aug',
+                                        'Sep',
+                                        'Oct',
+                                        'Nov',
+                                        'Dec'
+                                      ];
+                                      if (value.toInt() >= 7 &&
+                                          value.toInt() <= 11) {
+                                        return Text(
+                                          months[value.toInt() - 7],
+                                          style: const TextStyle(
+                                            fontSize: 10,
+                                            color: Colors.grey,
+                                          ),
+                                        );
+                                      }
+                                      return const SizedBox();
+                                    },
+                                    reservedSize: 30,
+                                    interval: 2,
+                                  ),
+                                ),
+                              ),
+                              borderData: FlBorderData(
+                                show: false,
+                                border: const Border(
+                                  bottom: BorderSide(color: Colors.black),
+                                  left: BorderSide.none,
+                                  top: BorderSide.none,
+                                  right: BorderSide.none,
+                                ),
+                              ),
+                              lineBarsData: [
+                                LineChartBarData(
+                                  spots: [
+                                    const FlSpot(7, 85),
+                                    const FlSpot(8, 100),
+                                    const FlSpot(9, 108),
+                                    const FlSpot(10, 200),
+                                    const FlSpot(11, 105.9),
+                                  ],
+                                  isCurved: true,
+                                  color: Colors.green,
+                                  barWidth: 4,
+                                  isStrokeCapRound: true,
+                                  belowBarData: BarAreaData(
+                                    show: true,
+                                    color: Colors.green.withOpacity(0.1),
+                                  ),
+                                ),
+                              ],
+                            ),
+                          )),
+                          const SizedBox(height: 10),
+                          SingleChildScrollView(
+                            scrollDirection: Axis.horizontal,
+                            child: Row(
+                              children: List.generate(5, (index) {
+                                return Padding(
+                                  padding:
+                                      const EdgeInsets.symmetric(horizontal: 8),
+                                  child: Text(
+                                    ['Aug', 'Sep', 'Oct', 'Nov', 'Dec'][index],
+                                    style: const TextStyle(
+                                      fontSize: 14,
+                                      fontWeight: FontWeight.w500,
+                                    ),
+                                  ),
+                                );
+                              }),
+                            ),
+                          )
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
+                Column(
+                  children: [
+                    Container(
+                      width: 100,
+                      height: 100,
+                      margin: const EdgeInsets.only(bottom: 10),
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(10),
+                        color: Colors.deepOrangeAccent,
+                      ),
+                      child: const Center(
+                        child: Text('hello'),
+                      ),
+                    ),
+                    Container(
+                      width: 100,
+                      height: 100,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(10),
+                        color: Colors.deepOrangeAccent,
+                      ),
+                      child: const Center(
+                        child: Text('hello'),
+                      ),
+                    ),
+                  ],
+                )
+              ],
+            ),
+            Container(
+              width: double.infinity,
+              height: 100,
+              decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(10),
+                  color: const Color(0xff07313A)),
+              child: const Padding(
+                padding: EdgeInsets.all(18.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Column(
+                      children: [
+                        Text(
+                          'Upgrade to Entreprise',
+                          style: TextStyle(
+                              fontSize: 26,
+                              color: Colors.white,
+                              fontWeight: FontWeight.bold),
+                          textAlign: TextAlign.start,
+                        ),
+                        Text(
+                          'Scaled your bussiness with pro feature.',
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 14,
+                          ),
+                          textAlign: TextAlign.start,
+                        ),
+                      ],
+                    ),
+                    Icon(
+                      Icons.arrow_forward_ios_sharp,
+                      color: Colors.white,
+                    )
+                  ],
+                ),
+              ),
+            ),
+            const Row(
+              children: [
+                Text(
+                  'Recent Activity',
+                  style: TextStyle(fontSize: 20, fontWeight: FontWeight.normal),
+                ),
+                Spacer(),
+                Icon(Icons.menu),
+              ],
+            ),
+            SingleChildScrollView(
+              scrollDirection: Axis.horizontal,
+              child: Row(
+                children: [
+                  createCustomCard(
+                    avatarImage: 'assets/avatar.jpg',
+                    name: 'Mario brozovic',
+                    email: 'BrazoM@gmail.com',
+                    amount: '€5,200',
+                    transactionId: '0023',
+                    date: '04 December 2021',
+                  ),
+                  createCustomCard(
+                    avatarImage: 'assets/avatar2.jpg',
+                    name: 'Luigi',
+                    email: 'luigi@example.com',
+                    amount: '€3,150',
+                    transactionId: '0024',
+                    date: '05 December 2021',
+                  ),
+                ],
+              ),
+            )
+          ],
+        ),
+      ),
+      bottomNavigationBar: StylishBottomBar(
+        option: AnimatedBarOptions(
+          barAnimation: BarAnimation.fade,
+          iconStyle: IconStyle.simple,
+          opacity: 0.3,
+        ),
+        backgroundColor: const Color(0xffECF0F1),
+        elevation: 0,
+        items: [
+          _buildStylishNavItem(Icons.home, 'Home', 0),
+          _buildStylishNavItem(Icons.copy, 'Search', 1),
+          BottomBarItem(
+            icon: GestureDetector(
+              onTap: () => {context.go('location')},
+              child: Container(
+                width: 50,
+                height: 50,
+                decoration: const BoxDecoration(
+                  color: Color(0xff07313A),
+                  shape: BoxShape.circle,
+                ),
+                child: const Icon(
+                  Icons.add,
+                  color: Colors.white,
+                  size: 30,
+                ),
+              ),
+            ),
+            title: const Text('Add'),
+            backgroundColor: Colors.orange,
+          ),
+          _buildStylishNavItem(Icons.graphic_eq, 'Messages', 3),
+          _buildStylishNavItem(Icons.person, 'Settings', 4),
+        ],
+        currentIndex: _selectedIndex,
+        onTap: (index) {
+          setState(() {
+            _selectedIndex = index;
+          });
+        },
+      ),
+    );
+  }
+
+  BottomBarItem _buildStylishNavItem(IconData icon, String title, int index) {
+    bool isSelected = _selectedIndex == index;
+
+    return BottomBarItem(
+      icon: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          Icon(
+            icon,
+            color: isSelected ? const Color(0xff2CD196) : Colors.grey,
+            size: 28,
+          ),
+          if (isSelected)
+            Container(
+              margin: const EdgeInsets.only(top: 4),
+              width: 6,
+              height: 6,
+              decoration: const BoxDecoration(
+                color: Color(0xff2CD196),
+                shape: BoxShape.circle,
+              ),
+            ),
+        ],
+      ),
+      title: Text(
+        title,
+        style: TextStyle(
+          color: isSelected ? const Color(0xff2CD196) : Colors.grey,
+        ),
+      ),
+      backgroundColor: Colors.transparent,
     );
   }
 }
